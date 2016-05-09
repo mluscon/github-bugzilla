d8b3af09f22d6c751b4f7e6468ad12b99977d445
Michal Luscon <mluscon@redhat.com>
2016-05-09 22:40:27 +0200

    add bugzilla connection

--- main.py
+++ main.py
@@ -3,12 +3,15 @@
 import re
 import sys
 
+import bugzilla
+
 from github import Github
 from configobj import ConfigObj
 
 config = ConfigObj('config')
 b_user = config.get('user')
 g_user, g_repo = tuple(sys.argv[1].split('/'))
+
 
 g = Github()
 
@@ -21,6 +24,11 @@ for pr in repo.get_pulls():
         tag = re.search('RhBug:[0-9,\s]+\)', headline)
         if tag:
             for bz_number in re.findall('[0-9]+', tag.group(0)):
-                bz_numbers.add((bz_number, pr.html_url))
+                bz_numbers.add((bz_number, pr))
 
-print(bz_numbers)
+bz = bugzilla.Bugzilla(url='https://bugzilla.redhat.com')
+
+for bz_number, pr_url in bz_numbers:
+    bug = bz.getbug(bz_number)
+    if bug.status in ['NEW', 'ASSIGNED']:
+        print(bug)
